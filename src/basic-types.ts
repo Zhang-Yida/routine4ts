abf9823bb114505bc811b0b13a1eef49e0777c61
张益达 <zhangm19910220@gmail.com>
2019-11-19 22:01:16 +0800

    Basic types doc completed

--- src/basic-types.ts
+++ src/basic-types.ts
@@ -68,6 +68,69 @@ export default function basicTypesConsole() {
    * any 与 Object 的不同请参考
    * https://stackoverflow.com/questions/18961203/any-vs-object
    */
-  
-   
+
+  // Void
+
+  /**
+   * 可为 void 类型的变量赋值 undefined 或 null
+   * strictNullChecks 如果为 true，则只能为 void 类型变量赋值 undefined
+   */
+  let voidVar: void = undefined;
+  console.log(voidVar);
+
+  function warningUser(): void {
+    console.log(`This is a warning message`);
+  }
+  warningUser();
+
+  // Undefined 和 Null
+
+  /**
+   * 默认情况下null和undefined是所有类型的子类型。
+   * 需要注意的是，当在 tsconfig 中的 strictNullChecks 配置项设置为 true 时，
+   * null 和 undefined 只能赋值给 any 类型变量和其各自类型的变量
+   */
+  /**
+   * null 和 undefined 都可以赋值给任何类型的变量
+   */
+  let undefinedVar: any = undefined;
+  console.log(undefinedVar);
+  let nullVar: any = null;
+  console.log(nullVar);
+
+  /**
+   * null 和 undefined 都可以赋值给其各自类型的变量
+   */
+  let undefinedType: undefined = undefined;
+  let nullType: null = null;
+  console.log(undefinedType, nullType);
+
+  // Never
+
+  // function error(params: string): never {
+  //   throw new Error(params);
+  // }
+
+  // error(`error message`);
+
+  // Object
+  function create(o: object | null): void {}
+  create({});
+  create(null);
+  /**
+   * object 类型是 number、string、boolean、bigint、symbol、null、undefined
+   * 以上类型之外的变量类型
+   * - wrong
+   * create(4);
+   * create(undefined);
+   */
+
+  // 类型断言
+  let someVal: any = `this is a string`;
+  let numVal: any = `4`;
+
+  // 类型断言并非是类型转换
+  let strLen: number = (someVal as string).length + (numVal as number);
+
+  console.log(strLen);
 }
