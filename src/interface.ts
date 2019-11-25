ef18b9131d96d6c6a5990589d4160049c2ba591d
zhangm19910220@gmail.com <zhangm19910220@gmail.com>
2019-11-25 11:26:09 +0800

    Interface Doc Completed

--- src/interface.ts
+++ src/interface.ts
@@ -81,5 +81,110 @@ export default function interfaceConsole(): void {
 
   console.log(func(``, 10));
 
-  // TODO: 可索引类型 等
+  // 可索引类型
+
+  /**
+   * 支持两种索引签名类型：字符串和数字
+   * TODO: 暂时不知道索引类型用来做什么，后续完善
+   */
+  interface StringArr {
+    [index: number]: string;
+    name: string;
+    age: number;
+  }
+
+  let strArr: StringArr = { name: `Zhang`, age: 20 };
+  console.log(strArr[0]);
+
+  // 类类型
+  /**
+   * 接口实现
+   */
+
+  interface IClock {
+    currentTime: Date;
+    setTime(d: Date): void;
+  }
+
+  class Clock implements IClock {
+    setTime(d: Date): void {
+      this.currentTime = d;
+    }
+    currentTime: Date;
+    constructor(h: string) {}
+  }
+
+  let clock = new Clock(`Test`);
+  clock.setTime(new Date());
+  console.log(clock.currentTime);
+
+  /**
+   * 类静态部分与实例部分的区别
+   * 当类实现接口时，只有实例部分能够被 Check，构造函数是静态部分，因此不会被 Check
+   */
+
+  interface INewClockCtor {
+    new (hour: number, min: number): INewClock;
+  }
+
+  interface INewClock {
+    tick(): void;
+  }
+
+  function createClock(ctor: INewClockCtor, h: number, m: number): INewClock {
+    return new ctor(h, m);
+  }
+
+  class DigitalClock implements INewClock {
+    constructor(h: number, m: number) {}
+    tick(): void {
+      console.log(`Beep`);
+    }
+  }
+  let digital = createClock(DigitalClock, 10, 20);
+  digital.tick();
+
+  /**
+   * 接口继承
+   */
+  interface Shape {
+    color: string;
+  }
+  interface Square extends Shape {
+    sideLength: number;
+  }
+
+  /** 类型断言的两种写法 */
+  let square = {} as Square;
+  let newSquare = <Square>{};
+  square.color = `red`;
+  square.sideLength = 200;
+  console.log(square);
+  console.log(newSquare);
+
+  // 接口继承类
+
+  /**
+   * 私有成员也会被接口继承
+   */
+  class Control {
+    // private state: any;
+  }
+  interface SelectableControl extends Control {
+    select(): void;
+  }
+  class Button extends Control implements SelectableControl {
+    select() {
+      console.log(`Button`);
+    }
+  }
+  // class Image implements SelectableControl {
+  //   select(): void {}
+  // }
+
+  let button = new Button();
+  console.log(button.select());
+
+  // let img = new Image();
+  // console.log(img.select());
 }
